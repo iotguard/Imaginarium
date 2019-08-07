f22d4963023b77a8d5a2c116d6a5e66c672ad329
iotguard <caowentao@iotguard.cn>
2019-08-07 15:22:19 +0800

    finish lecture 10

---
+++ Imaginarium/ImageViewController.h
@@ -0,0 +1,13 @@
+//
+//  ImageViewController.h
+//  Imaginarium
+//
+//  Created by dianyi on 2019/8/7.
+//  Copyright © 2019 dianyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageViewController : UIViewController
+@property (strong, nonatomic) NSURL *imageURL;
+@end
--- Imaginarium/ViewController.m
+++ Imaginarium/ViewController.m
@@ -7,17 +7,27 @@
 //
 
 #import "ViewController.h"
+#import "ImageViewController.h"
 
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if ([segue.destinationViewController isKindOfClass:[ImageViewController class]]) {
+        ImageViewController *ivc = (ImageViewController *)segue.destinationViewController;
+        NSString *url = @"https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo/bd_logo1_31bdc765.png";
+        if ([segue.identifier isEqualToString:@"photo_2"]) {
+            url = @"https://assets.burberry.com/is/image/Burberryltd/scarf_preco_exit2.jpg?$BBY_V2_BASIC=&qty=80,1$&wid=569";
+        } else if ([segue.identifier isEqualToString:@"photo_3"]) {
+            url = @"https://assets.burberry.com/is/image/Burberryltd/E247F912-7E40-488C-8967-1EF353ACE77B.jpg?$BBY_V2_BASIC=&qty=80,1$&wid=569";
+        }
+        
+        ivc.imageURL = [NSURL URLWithString:url];
+        ivc.title = segue.identifier;
+    }
 }
-
 
 @end
